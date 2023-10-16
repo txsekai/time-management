e4ecc342df83f727cbd9b5aa265a0be031f51d81
txsekai <109408172+txsekai@users.noreply.github.com>
2023-10-16 11:35:20 +0800

    dateItem el-dropdown change to el-select

--- src/view/ListPage/Components/DateItem.vue
+++ src/view/ListPage/Components/DateItem.vue
@@ -11,7 +11,8 @@
         <el-calendar
                 v-model="dateValue"
         ></el-calendar>
-        <el-button class="open-time-dialog-button button-padding" @click="timeDialogVisible=true">选择具体时间</el-button>
+        <el-button class="open-time-dialog-button button-padding" @click="timeDialogVisible=true">选择具体时间
+        </el-button>
         <!--     TODO title从calendar所选的取    -->
         <el-dialog
                 title="请选择具体时间"
@@ -31,75 +32,86 @@
                 <el-col :span="12">
                     <span class="content-font-size">完成时间 </span>
                     <el-switch
-                        style="width: 3.6rem"
-                        v-model="switchValue"
-                        active-color="#cf711f"
-                        @change="handleSwitch"
+                            style="width: 3.6rem"
+                            v-model="switchValue"
+                            active-color="#cf711f"
+                            @change="handleSwitch"
                     ></el-switch>
                 </el-col>
             </el-row>
 
-            <el-row class="mt6">
-                <el-col :span="12">
-                    <el-dropdown trigger="click">
-                        <el-button class="time-padding">
-                            {{ startCurrentHour }}
-                            <i class="el-icon-arrow-down el-icon--right"/>
-                        </el-button>
-                        <el-dropdown-menu slot="dropdown">
-                            <el-dropdown-item v-for="hour in hours" :key="hour">{{ hour }}</el-dropdown-item>
-                        </el-dropdown-menu>
-                    </el-dropdown>
-                    <span> : </span>
-                    <el-dropdown trigger="click">
-                        <el-button class="time-padding">
-                            {{ startCurrentMinute }}
-                            <i class="el-icon-arrow-down el-icon--right"/>
-                        </el-button>
-                        <el-dropdown-menu slot="dropdown">
-                            <el-dropdown-item v-for="minute in minutes" :key="minute">{{ minute }}</el-dropdown-item>
-                        </el-dropdown-menu>
-                    </el-dropdown>
+            <el-row class="mt3">
+                <el-col :span="6">
+                    <div class="time-select">
+                        <el-select v-model="startHour">
+                            <el-option
+                                    v-for="hour in hours"
+                                    :key="hour"
+                                    :label="hour"
+                                    :value="hour"
+                            ></el-option>
+                        </el-select>
+                        <span> : </span>
+                        <el-select v-model="startMinute">
+                            <el-option
+                                    v-for="minute in minutes"
+                                    :key="minute"
+                                    :label="minute"
+                                    :value="minute"
+                            ></el-option>
+                        </el-select>
+                    </div>
                 </el-col>
 
-                <el-col :span="12" v-show="completedVisible">
-                    <el-dropdown trigger="click">
-                        <el-button class="time-padding">
-                            {{ completedCurrentHour }}
-                            <i class="el-icon-arrow-down el-icon--right"/>
-                        </el-button>
-                        <el-dropdown-menu slot="dropdown">
-                            <el-dropdown-item v-for="hour in hours" :key="hour">{{ hour }}</el-dropdown-item>
-                        </el-dropdown-menu>
-                    </el-dropdown>
-                    <span> : </span>
-                    <el-dropdown trigger="click">
-                        <el-button class="time-padding">
-                            {{ completedCurrentMinute }}
-                            <i class="el-icon-arrow-down el-icon--right"/>
-                        </el-button>
-                        <el-dropdown-menu slot="dropdown">
-                            <el-dropdown-item v-for="minute in minutes" :key="minute">{{ minute }}</el-dropdown-item>
-                        </el-dropdown-menu>
-                    </el-dropdown>
+                <el-col :span="6" style="min-height: 1rem"></el-col>
+
+                <el-col :span="6" v-show="completedVisible">
+                    <div class="time-select">
+                        <el-select v-model="completedHour">
+                            <el-option
+                                    v-for="hour in hours"
+                                    :key="hour"
+                                    :label="hour"
+                                    :value="hour"
+                            ></el-option>
+                        </el-select>
+                        <span> : </span>
+                        <el-select v-model="completedMinute">
+                            <el-option
+                                    v-for="minute in minutes"
+                                    :key="minute"
+                                    :label="minute"
+                                    :value="minute"
+                            ></el-option>
+                        </el-select>
+                    </div>
                 </el-col>
             </el-row>
 
             <el-row class="mt16">
                 <el-col :span="12" style="min-height: 1rem">
-                    <span class="content-font-size" v-if="completedVisible">所用时长：{{ spendTime }}</span>
+                    <span class="content-font-size" v-show="completedVisible">所用时长：{{ spendTime }}</span>
                 </el-col>
             </el-row>
 
             <div class="spend-time-grid mt16">
-                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">10分钟</el-button>
-                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">15分钟</el-button>
-                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">20分钟</el-button>
-                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime" ref="defaultSelectTime">30分钟</el-button>
-                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">40分钟</el-button>
-                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">60分钟</el-button>
-                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">90分钟</el-button>
-                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">120分钟</el-button>
+                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">10分钟
+                </el-button>
+                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">15分钟
+                </el-button>
+                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">20分钟
+                </el-button>
+                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime"
+                           ref="defaultSelectTime">30分钟
+                </el-button>
+                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">40分钟
+                </el-button>
+                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">60分钟
+                </el-button>
+                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">90分钟
+                </el-button>
+                <el-button class="spend-time-padding" :disabled="completedStatus" @click="handleSpendTime">120分钟
+                </el-button>
             </div>
 
             <div slot="footer" class="dialog-footer">
@@ -129,14 +141,12 @@ export default {
         return {
             dateValue: new Date(),
             timeDialogVisible: false,
-            startTime: "",
-            endTime: "",
-            startCurrentHour: '',
+            startHour: '',
+            startMinute: '',
             hours: [],
-            startCurrentMinute: '',
             minutes: [],
-            completedCurrentHour: '',
-            completedCurrentMinute: '',
+            completedHour: '',
+            completedMinute: '',
             spendTime: '',
             switchValue: false,
             completedStatus: true,
@@ -146,14 +156,14 @@ export default {
 
     created() {
         const startCurrentDateTime = new Date()
-        this.startCurrentHour = startCurrentDateTime.getHours().toString().padStart(2, '0')
+        this.startHour = startCurrentDateTime.getHours().toString().padStart(2, '0')
 
         for (let i = 0; i <= 23; i++) {
             const hour = i.toString().padStart(2, '0')
             this.hours.push(hour)
         }
 
-        this.startCurrentMinute = startCurrentDateTime.getMinutes().toString().padStart(2, '0')
+        this.startMinute = startCurrentDateTime.getMinutes().toString().padStart(2, '0')
 
         for (let i = 0; i <= 59; i++) {
             const minute = i.toString().padStart(2, '0')
@@ -169,10 +179,10 @@ export default {
             this.$emit("cancel")
         },
         handleSwitch() {
-            if(this.switchValue) {
+            if (this.switchValue) {
                 this.completedVisible = true
                 this.completedStatus = false
-            }else {
+            } else {
                 this.completedVisible = false
                 this.completedStatus = true
             }
@@ -189,14 +199,14 @@ export default {
             const numericValue = buttonText.slice(0, -2)
             this.spendTime = buttonText
 
-            const startHour = this.startCurrentHour
-            const startMinute = this.startCurrentMinute
+            const startHour = this.startHour
+            const startMinute = this.startMinute
             const startTime = new Date()
             startTime.setHours(startHour)
             startTime.setMinutes(startMinute)
             const completedTime = new Date(startTime.getTime() + numericValue * 60000)  // 将分钟转换为毫秒
-            this.completedCurrentHour = completedTime.getHours().toString().padStart(2, '0')
-            this.completedCurrentMinute = completedTime.getMinutes().toString().padStart(2, '0')
+            this.completedHour = completedTime.getHours().toString().padStart(2, '0')
+            this.completedMinute = completedTime.getMinutes().toString().padStart(2, '0')
         }
     },
 }
@@ -207,7 +217,9 @@ export default {
 /deep/ .el-calendar,
 /deep/ .el-button,
 /deep/ .el-dropdown-menu__item, .el-menu-item,
-.content-font-size {
+.content-font-size,
+/deep/ .el-input,
+/deep/ .el-select .el-input .el-select__caret {
     font-size: 62.5%;
 }
 
@@ -233,26 +245,35 @@ export default {
     margin-left: 2rem;
 }
 
-.el-dropdown-menu {
-    height: 36rem;
-    overflow: scroll;
-    padding: 0.5rem 0;
+.time-select {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
 }
 
-/deep/ .el-dropdown-menu__item {
+/deep/ .el-input--suffix .el-input__inner {
+    padding-right: 1rem;
+}
+
+/deep/ .el-input__inner {
+    height: 2rem;
+    line-height: 2rem;
+    padding: 0 0.75rem;
+}
+
+/deep/ .el-input__icon {
     line-height: 2rem;
 }
 
-/deep/ .el-dropdown-menu__item, .el-menu-item {
+.el-select-dropdown__item {
+    font-size: 62.5%;
     padding: 0 1rem;
+    height: 2rem;
+    line-height: 2rem;
 }
 
-.el-popper[x-placement^=bottom] {
-    margin-top: 0.6rem;
-}
-
-.time-padding {
-    padding: 0.3rem 0.6rem;
+.el-input__icon {
+    width: 2rem;
 }
 
 .spend-time-grid {
